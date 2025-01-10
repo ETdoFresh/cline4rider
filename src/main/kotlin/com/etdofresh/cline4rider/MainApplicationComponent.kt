ac0b2248bc0f6e57620bdc111b79c95b4b7fe86a
ETdoFresh <ETdoFresh@gmail.com>
2025-01-10 15:10:33 -0600

    Migrate MainApplicationComponent to cline4rider package

---
+++ src/main/kotlin/com/etdofresh/cline4rider/MainApplicationComponent.kt
@@ -0,0 +1,20 @@
+package com.etdofresh.cline4rider
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+
+@Service(Service.Level.PROJECT)
+class MainApplicationComponent(private val project: Project) : StartupActivity {
+    override fun runActivity(project: Project) {
+        // Initialize project-level services
+        project.messageBus.connect().subscribe(
+            ClineMessageHandler.TASK_REQUEST,
+            object : TaskRequestListener {
+                override fun onTaskRequest(message: com.etdofresh.cline4rider.model.ClineMessage) {
+                    // Handle task request
+                }
+            }
+        )
+    }
+}
