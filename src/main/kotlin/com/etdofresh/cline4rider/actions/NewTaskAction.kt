01dbe927ff932f1784bbe64f30765585eda630e3
ETdoFresh <ETdoFresh@gmail.com>
2025-01-10 13:59:15 -0600

    Migrate NewTaskAction to cline4rider package

---
+++ src/main/kotlin/com/etdofresh/cline4rider/actions/NewTaskAction.kt
@@ -0,0 +1,10 @@
+package com.etdofresh.cline4rider.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class NewTaskAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        // TODO: Implement new task functionality
+    }
+}
