c78965d5a3ca322457c18bf9e84e602e67b9292d
ETdoFresh <ETdoFresh@gmail.com>
2025-01-10 13:58:58 -0600

    Migrate ClearChatAction to cline4rider package

---
+++ src/main/kotlin/com/etdofresh/cline4rider/actions/ClearChatAction.kt
@@ -0,0 +1,10 @@
+package com.etdofresh.cline4rider.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ClearChatAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        // TODO: Implement clear chat functionality
+    }
+}
